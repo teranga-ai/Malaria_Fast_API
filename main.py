ade77b048e309926d4fa222f1fa2b50f6037b676
Teranga-ai_Chat <129336487+teranga-ai@users.noreply.github.com>
2025-04-20 00:31:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,3 @@ async def classify_image(file: UploadFile = File(...)):
 @app.get("/")
 def home():
     return {"message": "Bienvenue sur l'API de classification du paludisme"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-
